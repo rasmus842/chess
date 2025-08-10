438def2c6cbabe226d27da42eeafd487967e257d
rasmus842 <rasmuszalite@gmail.com>
2025-08-10 11:20:29 +0300

    Work on UI a bit

--- lib/chess_web/router.ex
+++ lib/chess_web/router.ex
@@ -6,6 +6,7 @@ defmodule ChessWeb.Router do
     plug :fetch_session
     plug :fetch_live_flash
     plug :put_root_layout, html: {ChessWeb.Layouts, :root}
+    plug :put_layout, html: {ChessWeb.Layouts, :app}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
