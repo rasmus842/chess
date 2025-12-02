e00f8e1ad48a28ec6fbb60ce34b7db9a06859f3a
rasmus842 <rasmuszalite@gmail.com>
2025-12-02 08:16:32 +0200

    Pawn moves test

---
+++ test/chess/game/pawn_move_test.exs
@@ -0,0 +1,94 @@
+defmodule Chess.Game.PawnMoveTest do
+  use Chess.Game.MoveCase
+
+  describe "Pawn movements" do
+    setup do
+      board = %{
+        {?a, 2} => {:white, :pawn},
+        {?a, 7} => {:black, :pawn},
+        {?g, 7} => {:black, :pawn},
+        {?g, 6} => {:black, :pawn}
+      }
+
+      {:ok, %{board: board}}
+    end
+
+    test "white pawn moves forward one step correctly", %{board: board} do
+      move = {{?a, 2}, {?a, 3}}
+      assert_move_is_ok(board, move)
+    end
+
+    test "black pawn moves forward one step correctly", %{board: board} do
+      move = {{?a, 7}, {?a, 6}}
+      assert_move_is_ok(board, move)
+    end
+
+    test "pawn cannot move backward", %{board: board} do
+      move = {{?a, 2}, {?a, 1}}
+      assert_move_is_error(board, move)
+    end
+
+    test "pawn cannot move sideways", %{board: board} do
+      move = {{?a, 2}, {?b, 2}}
+      assert_move_is_error(board, move)
+    end
+
+    test "pawn cannot move diagonally if not taking 1", %{board: board} do
+      move = {{?a, 2}, {?b, 3}}
+      assert_move_is_error(board, move)
+    end
+
+    test "pawn cannot move diagonally if not taking 2", %{board: board} do
+      move = {{?a, 7}, {?b, 6}}
+      assert_move_is_error(board, move)
+    end
+
+    test "pawn jump", %{board: board} do
+      move = {{?a, 2}, {?a, 4}}
+      assert_move_is_error(board, move)
+    end
+
+    test "pawn cannot jump if blocked", %{board: board} do
+      move = {{?g, 7}, {?g, 5}}
+      assert_move_is_error(board, move)
+    end
+  end
+
+  describe "Pawn takes" do
+    setup do
+      board = %{
+        {?a, 2} => {:white, :pawn},
+        {?b, 4} => {:black, :pawn},
+        {?d, 4} => {:white, :pawn},
+        {?f, 5} => {:black, :pawn},
+        {?h, 4} => {:white, :pawn},
+        {?h, 5} => {:black, :pawn}
+      }
+
+      {:ok, %{board: board}}
+    end
+
+    test "White pawn takes black piece", %{board: board} do
+      move = {{?d, 4}, {?f, 5}}
+      assert_move_is_ok(board, move)
+    end
+
+    test "Black pawn takes white piece", %{board: board} do
+      move = {{?f, 5}, {?d, 4}}
+      assert_move_is_ok(board, move)
+    end
+
+    test "pawn cannot take forwards", %{board: board} do
+      move = {{?h, 4}, {?h, 5}}
+      assert_move_is_error(board, move)
+    end
+
+    test "En passant - black pawn takes white", %{board: board} do
+      white_pawn_jumps = {{?a, 2}, {?a, 4}}
+      new_state = assert_move_is_ok(board, white_pawn_jumps)
+      assert {%{player: :black}, new_board} = new_state
+      black_pawn_takes = {{?b, 4}, {?a, 3}}
+      assert_move_is_ok(new_board, black_pawn_takes)
+    end
+  end
+end
